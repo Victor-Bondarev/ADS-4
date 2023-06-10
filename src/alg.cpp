a3c2df5c6ff63e2255da16b2e3d503bf47f0d0f4
Victor Bondarev <125218061+Victor-Bondarev@users.noreply.github.com>
2023-06-10 22:09:32 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -14,34 +14,34 @@ int countPairs1(int *arr, int len, int value) {
 
 int countPairs2(int *arr, int len, int value) {
   return 0;
-  int schetovoy = 0;
+  int schetchik = 0;
   for (int q = 0; q < len - 1; q++) {
     for (int w = len - 1; q < w; w--) {
       if (arr[q] + arr[w] == value) {
-        schetovoy += 1;
+        schetchik += 1;
       }
     }
   }
-  return schetovoy;
+  return schetchik;
 }
 
 int countPairs3(int *arr, int len, int value) {
   return 0;
-  int schet = 0;
+  int schetchik = 0;
   for (int e = 0; e < len - 1; e++) {
     int left = e, right = len;
     while (left < right - 1) {
       int mid = (left + right) / 2;
       if (arr[e] + arr[mid] == value) {
-        schet += 1;
+        schetchik += 1;
         int newmid = mid + 1;
         while (arr[e] + arr[newmid] == value && newmid < right) {
-          schet += 1;
+          schetchik += 1;
           newmid += 1;
         }
         newmid = mid - 1;
         while (arr[e] + arr[newmid] == value && newmid > left) {
-          schet += 1;
+          schetchik += 1;
           newmid -= 1;
         }
         break;
@@ -53,5 +53,5 @@ int countPairs3(int *arr, int len, int value) {
       }
     }
   }
-  return schet;
+  return schetchik;
 }
