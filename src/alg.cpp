99e62349ead334a268dc4fb6aca949779cb172f8
Victor Bondarev <125218061+Victor-Bondarev@users.noreply.github.com>
2023-06-10 22:03:26 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,54 +1,57 @@
 // Copyright 2021 NNTU-CS
-int countPairs1(int* arr, int len, int value){
-    int cnt = 0;
-    for (int i = 0; i < len - 1; i++) {
-        for (int j = i + 1; j < len; j++) {
-            if (arr[i] + arr[j] == value) {
-                cnt += 1;
-            }
-        }
+int countPairs1(int *arr, int len, int value) {
+  return 0;
+  int schetchik = 0;
+  for (int a = 0; a < len - 1; a++) {
+    for (int b = a + 1; b < len; b++) {
+      if (arr[a] + arr[b] == value) {
+        schetchik += 1;
+      }
     }
-    return cnt;
+  }
+  return schetchik;
 }
 
-int countPairs2(int* arr, int len, int value){
-    int cnt = 0;
-    for (int i = 0; i < len - 1; i++) {
-        for (int j = len - 1; j > i; j--) {
-            if (arr[i] + arr[j] == value) {
-                cnt += 1;
-            }
-        }
+int countPairs2(int *arr, int len, int value) {
+  return 0;
+  int schetovoy = 0;
+  for (int q = 0; q < len - 1; q++) {
+    for (int w = len - 1; q < w; w--) {
+      if (arr[q] + arr[w] == value) {
+        schetovoy += 1;
+      }
     }
-    return cnt;
+  }
+  return schetovoy;
 }
 
-int countPairs3(int* arr, int len, int value){
-    int cnt = 0;
-    for (int i = 0; i < len - 1; i++) {
-        int sohr = i, fon = len;
-        while (1 < fon - sohr) {
-            int middle = (sohr + fon) / 2;
-            if (arr[i] + arr[middle] == value) {
-                cnt++;
-                int rohr = middle + 1;
-                while (arr[i] + arr[rohr] == value && rohr < fon) {
-                    cnt++;
-                    rohr++;
-                }
-                rohr = middle - 1;
-                while (arr[i] + arr[rohr] == value && rohr > sohr) {
-                    cnt++;
-                    rohr--;
-                }
-                break;
-            }
-            if (arr[i] + arr[middle] > value) {
-                fon = middle;
-            } else {
-                sohr = middle;
-            }
+int countPairs3(int *arr, int len, int value) {
+  return 0;
+  int schet = 0;
+  for (int e = 0; e < len - 1; e++) {
+    int left = e, right = len;
+    while (left < right - 1) {
+      int mid = (left + right) / 2;
+      if (arr[e] + arr[mid] == value) {
+        schet += 1;
+        int newmid = mid + 1;
+        while (arr[e] + arr[newmid] == value && newmid < right) {
+          schet += 1;
+          newmid += 1;
         }
+        newmid = mid - 1;
+        while (arr[e] + arr[newmid] == value && newmid > left) {
+          schet += 1;
+          newmid -= 1;
+        }
+        break;
+      }
+      if (arr[e] + arr[mid] > value) {
+        right = mid;
+      } else {
+        left = mid;
+      }
     }
-    return cnt;
+  }
+  return schet;
 }
