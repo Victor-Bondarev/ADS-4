b0d6f9f86fe3fd3f539191fb4ebb4cbf6c947960
Victor Bondarev <125218061+Victor-Bondarev@users.noreply.github.com>
2023-06-10 22:12:07 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -15,9 +15,9 @@ int countPairs1(int *arr, int len, int value) {
 int countPairs2(int *arr, int len, int value) {
   return 0;
   int schetchik = 0;
-  for (int q = 0; q < len - 1; q++) {
-    for (int w = len - 1; q < w; w--) {
-      if (arr[q] + arr[w] == value) {
+  for (int a = 0; a < len - 1; a++) {
+    for (int b = len - 1; a < b; b--) {
+      if (arr[a] + arr[b] == value) {
         schetchik += 1;
       }
     }
@@ -28,25 +28,25 @@ int countPairs2(int *arr, int len, int value) {
 int countPairs3(int *arr, int len, int value) {
   return 0;
   int schetchik = 0;
-  for (int e = 0; e < len - 1; e++) {
-    int left = e, right = len;
+  for (int a = 0; a < len - 1; a++) {
+    int left = a, right = len;
     while (left < right - 1) {
       int mid = (left + right) / 2;
-      if (arr[e] + arr[mid] == value) {
+      if (arr[a] + arr[mid] == value) {
         schetchik += 1;
         int newmid = mid + 1;
-        while (arr[e] + arr[newmid] == value && newmid < right) {
+        while (arr[a] + arr[newmid] == value && newmid < right) {
           schetchik += 1;
           newmid += 1;
         }
         newmid = mid - 1;
-        while (arr[e] + arr[newmid] == value && newmid > left) {
+        while (arr[a] + arr[newmid] == value && newmid > left) {
           schetchik += 1;
           newmid -= 1;
         }
         break;
       }
-      if (arr[e] + arr[mid] > value) {
+      if (arr[a] + arr[mid] > value) {
         right = mid;
       } else {
         left = mid;
