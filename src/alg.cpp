b8d139d5e338a87cbc903e713568d5becd514706
Victor Bondarev <125218061+Victor-Bondarev@users.noreply.github.com>
2023-06-10 22:14:53 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,57 +1,57 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
   return 0;
-  int schetchik = 0;
-  for (int a = 0; a < len - 1; a++) {
-    for (int b = a + 1; b < len; b++) {
-      if (arr[a] + arr[b] == value) {
-        schetchik += 1;
+  int count = 0;
+  for (int i = 0; i < len - 1; i++) {
+    for (int j = i + 1; j < len; j++) {
+      if (arr[i] + arr[j] == value) {
+        count += 1;
       }
     }
   }
-  return schetchik;
+  return count;
 }
 
 int countPairs2(int *arr, int len, int value) {
   return 0;
-  int schetchik = 0;
-  for (int a = 0; a < len - 1; a++) {
-    for (int b = len - 1; a < b; b--) {
-      if (arr[a] + arr[b] == value) {
-        schetchik += 1;
+  int count = 0;
+  for (int i = 0; i < len - 1; i++) {
+    for (int j = len - 1; i < j; j--) {
+      if (arr[i] + arr[j] == value) {
+        count += 1;
       }
     }
   }
-  return schetchik;
+  return count;
 }
 
 int countPairs3(int *arr, int len, int value) {
   return 0;
-  int schetchik = 0;
-  for (int a = 0; a < len - 1; a++) {
-    int left = a, right = len;
+  int count = 0;
+  for (int i = 0; i < len - 1; i++) {
+    int left = i, right = len;
     while (left < right - 1) {
       int mid = (left + right) / 2;
-      if (arr[a] + arr[mid] == value) {
-        schetchik += 1;
+      if (arr[i] + arr[mid] == value) {
+        count += 1;
         int newmid = mid + 1;
-        while (arr[a] + arr[newmid] == value && newmid < right) {
-          schetchik += 1;
+        while (arr[i] + arr[newmid] == value && newmid < right) {
+          count += 1;
           newmid += 1;
         }
         newmid = mid - 1;
-        while (arr[a] + arr[newmid] == value && newmid > left) {
-          schetchik += 1;
+        while (arr[i] + arr[newmid] == value && newmid > left) {
+          count += 1;
           newmid -= 1;
         }
         break;
       }
-      if (arr[a] + arr[mid] > value) {
+      if (arr[i] + arr[mid] > value) {
         right = mid;
       } else {
         left = mid;
       }
     }
   }
-  return schetchik;
+  return count;
 }
